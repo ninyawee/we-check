56e36c96409d0099f0296923f05b54bfa99f9b12
Phun Peeticharoenthum <phunphun484@gmail.com>
2026-01-17 20:53:32 +0700

    edit: move accuracy badge to top left

--- src/components/forms/locationInfoForm/index.tsx
+++ src/components/forms/locationInfoForm/index.tsx
@@ -42,78 +42,78 @@ const LocationInfoForm: FC = () => {
           padding={"1rem"}
           position={"relative"}
         >
-          <Stack
-            direction={"column"}
-            justifyContent={"space-between"}
-            maxWidth={"80%"}
-          >
-            <Stack direction={"row"} alignItems={"center"}>
+          <Stack direction={"row"} alignItems={"flex-start"} maxWidth={"80%"}>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center",
+                backgroundColor: "#000000",
+                color: "#000",
+                width: "3.5rem",
+                height: "3.5rem",
+                borderRadius: "0.5rem",
+                padding: "0.25rem",
+                boxShadow: "0 1px 3px rgba(0,0,0,0.2)",
+                border: `1px solid ${gradeColor}`,
+                marginRight: "0.75rem",
+              }}
+            >
               <Typography
-                fontSize={"1.125rem"}
-                sx={{ wordWrap: "break-word", color: "#FFFFFF" }}
+                fontSize={"0.75rem"}
+                fontWeight={200}
+                sx={{ lineHeight: 1, textAlign: "center", color: gradeColor }}
               >
-                {selectedLocation?.unitName}
-                {((selectedLocation?.year &&
-                  Number(selectedLocation.year) !== new Date().getFullYear()) ||
-                  !selectedLocation?.year) && (
-                  <Typography
-                    component="span"
-                    fontSize={"0.7rem"}
-                    sx={{
-                      backgroundColor: "#424242",
-                      color: "#FFF",
-                      padding: "0.125rem 0.5rem",
-                      borderRadius: "0.5rem",
-                      marginLeft: "0.5rem",
-                      display: "inline-block",
-                      verticalAlign: 'middle'
-                    }}
-                  >
-                    {selectedLocation?.year
-                      ? `ข้อมูลเก่าปี ${selectedLocation.year}`
-                      : "ข้อมูลเก่า"}
-                  </Typography>
-                )}
+                แม่นยำ
               </Typography>
-            </Stack>
-            <Stack direction={"row"} marginBottom={"0.875rem"}>
-              <Typography fontSize={"0.8rem"} color="#A4A4A4">
-                {`หน่วย ${selectedLocation?.unitNumber} ${selectedLocation?.subDistrictName} เขต ${selectedLocation?.divisionNumber} ${selectedLocation?.provinceName}`}
+              <Typography
+                fontSize={"1.25rem"}
+                fontWeight={700}
+                sx={{ lineHeight: 1, marginTop: "0.3rem", textAlign: "center", color: gradeColor }}
+              >
+                {gradeLabel}
               </Typography>
+            </Box>
+
+            <Stack direction={"column"} justifyContent={"space-between"} flex={1}>
+              <Stack direction={"row"} alignItems={"center"}>
+                <Typography
+                  fontSize={"1.125rem"}
+                  sx={{ wordWrap: "break-word", color: "#FFFFFF" }}
+                >
+                  {selectedLocation?.unitName}
+                  {((selectedLocation?.year &&
+                    Number(selectedLocation.year) !== new Date().getFullYear()) ||
+                    !selectedLocation?.year) && (
+                    <Typography
+                      component="span"
+                      fontSize={"0.7rem"}
+                      sx={{
+                        backgroundColor: "#424242",
+                        color: "#FFF",
+                        padding: "0.125rem 0.5rem",
+                        borderRadius: "0.5rem",
+                        marginLeft: "0.5rem",
+                        display: "inline-block",
+                        verticalAlign: 'middle'
+                      }}
+                    >
+                      {selectedLocation?.year
+                        ? `ข้อมูลเก่าปี ${selectedLocation.year}`
+                        : "ข้อมูลเก่า"}
+                    </Typography>
+                  )}
+                </Typography>
+              </Stack>
+              <Stack direction={"row"} marginBottom={"0.875rem"}>
+                <Typography fontSize={"0.8rem"} color="#A4A4A4">
+                  {`หน่วย ${selectedLocation?.unitNumber} ${selectedLocation?.subDistrictName} เขต ${selectedLocation?.divisionNumber} ${selectedLocation?.provinceName}`}
+                </Typography>
+              </Stack>
             </Stack>
           </Stack>
-          {/* Location grade badge on the top-right */}
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              justifyContent: "center",
-              backgroundColor: "#000000",
-              color: "#000",
-              width: "3.5rem",
-              height: "3.5rem",
-              borderRadius: "0.5rem",
-              padding: "0.25rem",
-              boxShadow: "0 1px 3px rgba(0,0,0,0.2)",
-              border: `1px solid ${gradeColor}`,
-            }}
-          >
-            <Typography
-              fontSize={"0.75rem"}
-              fontWeight={200}
-              sx={{ lineHeight: 1, textAlign: "center", color: gradeColor }}
-            >
-              แม่นยำ
-            </Typography>
-            <Typography
-              fontSize={"1.25rem"}
-              fontWeight={700}
-              sx={{ lineHeight: 1, marginTop: "0.3rem", textAlign: "center", color: gradeColor }}
-            >
-              {gradeLabel}
-            </Typography>
-          </Box>
+          {/* badge moved into the left header (before unit name) */}
         </Stack>
         <Stack
           direction="column"
